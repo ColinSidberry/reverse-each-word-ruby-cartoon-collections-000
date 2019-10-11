2dd7e5ea6e44637a01b7e47c49e9686b935a7b39
Colin Sidberry <colinsidberry@gmail.com>
2019-10-11 15:39:36 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,13 @@
+def reverse_each_word1(words)
+  words = words.split(" ")
+  words.each.with_index do |word,i|
+    words[i] = word.reverse
+  end
+  return words.join(" ")
+end
+
+def reverse_each_word2(words)
+  words = words.split(" ")
+  words = words.collect {|word| word.reverse}
+  return words.join(" ")
+end
